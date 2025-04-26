dc9801e66e627b4627fbd7862b73da12f9de8142
Atmaram Naik <naik.atmaram@gmail.com>
2025-04-26 20:07:11 +0530

    add more providers

--- src/core/lib.ts
+++ src/core/lib.ts
@@ -1,11 +1,13 @@
-import { ChatAnthropic } from "@langchain/anthropic";
+import { AnthropicInput, ChatAnthropic } from "@langchain/anthropic";
 import { BaseLanguageModelInput } from "@langchain/core/language_models/base";
-import { BaseChatModelCallOptions } from "@langchain/core/language_models/chat_models.js";
+import { BaseChatModelCallOptions, BaseChatModelParams } from "@langchain/core/language_models/chat_models.js";
 import { AIMessageChunk } from "@langchain/core/messages";
 import { Runnable } from "@langchain/core/runnables";
 import { StructuredTool } from "@langchain/core/tools";
-import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatGoogleGenerativeAI, GoogleGenerativeAIChatInput } from "@langchain/google-genai";
+import { AzureChatOpenAI, AzureOpenAIInput, ChatOpenAI, ChatOpenAIFields, ClientOptions, OpenAIChatInput } from "@langchain/openai";
 import { Page } from "playwright-core";
+
 import { z } from "zod";
 class NavigateTool extends StructuredTool {
     schema = z.object({
@@ -33,6 +35,21 @@ class ClickTool extends StructuredTool {
 
     async _call(args: z.infer<this["schema"]>): Promise<string> {
         return "click_on_element"
+    }
+}
+class DragDropTool extends StructuredTool {
+    schema = z.object({
+        sourceRef: z.string().describe('Exact source element reference from the page snapshot which needs to be dragged'),
+        targetRef: z.string().describe('Exact target element reference from the page snapshot where source element needs to be dropped'),
+    });
+
+    name = "drag_element_and_drop_on_element";
+
+    description =
+        "This tool drags source element and drop it to target element";
+
+    async _call(args: z.infer<this["schema"]>): Promise<string> {
+        return "drag_element_and_drop_on_element"
     }
 }
 class FillTextTool extends StructuredTool {
@@ -65,35 +82,68 @@ class CheckIfVisible extends StructuredTool {
     }
 
 }
-
+type GoogleModelInitializationParams = {
+    provider: "google";
+    fields?: GoogleGenerativeAIChatInput;
+}
+type AnthropicModelInitializationParams = {
+    provider: "anthropic";
+    fields?: AnthropicInput & BaseChatModelParams;
+}
+type AzureOpenAIModelInitializationParams = {
+    provider: "azure-openai";
+    fields?: Partial<OpenAIChatInput> & Partial<AzureOpenAIInput> & {
+        openAIApiKey?: string;
+        openAIApiVersion?: string;
+        openAIBasePath?: string;
+        deploymentName?: string;
+    } & BaseChatModelParams & {
+        configuration?: ClientOptions;
+    }
+}
+type OpenAIModelInitializationParams = {
+    provider: "openai";
+    fields?: ChatOpenAIFields;
+}
+type ModelInitializationParams = AzureOpenAIModelInitializationParams | GoogleModelInitializationParams | AnthropicModelInitializationParams | OpenAIModelInitializationParams
 export class AI {
     private started: boolean
     private debug: boolean
     private page: Page
     private model: Runnable<BaseLanguageModelInput, AIMessageChunk, BaseChatModelCallOptions>
 
-    constructor(page: Page, provider = (process.env.PROVIDER_NAME || "google"), modelName = (process.env.MODEL_NAME || "gemini-2.0-flash"), debug = false) {
+    constructor(page: Page, modelInitializationParams: ModelInitializationParams, debug = false) {
         this.started = false
         this.debug = debug
         this.page = page;
-        switch (provider) {
+        const tools = [new NavigateTool(), new ClickTool(), new FillTextTool(), new CheckIfVisible(), new DragDropTool()]
+        switch (modelInitializationParams.provider) {
             case "google":
-                const model1 = new ChatGoogleGenerativeAI({
-                    model: modelName,
-                });
+                const model1 = new ChatGoogleGenerativeAI(modelInitializationParams.fields);
                 this.model = model1.bind({
-                    tools: [new NavigateTool(), new ClickTool(), new FillTextTool(), new CheckIfVisible()],
+                    tools,
                 });
                 break
             case "anthropic":
-                const model2 = new ChatAnthropic({
-                    model: modelName,
-                });
+                const model2 = new ChatAnthropic(modelInitializationParams.fields);
                 this.model = model2.bind({
-                    tools: [new NavigateTool(), new ClickTool(), new FillTextTool(), new CheckIfVisible()],
+                    tools,
                 });
+                break
+            case "azure-openai":
+                const model3 = new AzureChatOpenAI(modelInitializationParams.fields);
+                this.model = model3.bind({
+                    tools,
+                });
+                break
+            case "openai":
+                const model4 = new ChatOpenAI(modelInitializationParams.fields);
+                this.model = model4.bind({
+                    tools,
+                });
+                break
             default:
-                throw Error("Invalid provider name please set PROVIDER_NAME as either google or anthropic")
+                throw Error("Invalid provider")
         }
     }
     setPage(page: Page) {
@@ -101,31 +151,30 @@ export class AI {
     }
     private async tce(page: Page, prompt: string, pollingInterval: number = 1000, iterations: number = 20, attempt: number = 0, scrollIfRequired: boolean = false) {
         if (!this.started) {
-            // if(this.debug){
-            //     console.log("Sending System Prompt")
-            // }
+            if (this.debug) {
+                console.log("Sending System Prompt")
+            }
             await this.model.invoke([
                 "system",
-                "You are driver agent that drives automation specified in plain english, You have access to tools to accomplish steps provided subsequently during each interaction tools will execute code on playwright page and provide aria snapshot with of all elements"
+                "You are driver agent that drives automation specified in plain english, You have access to tools to accomplish steps provided subsequently during each interaction tools will execute code on playwright page and provide aria snapshot with of all elements your response should be only tool calls and no other text. You can send multiple tool calls in a single response."
             ])
             this.started = true
-            // if(this.debug){
-            //     console.log("Done Sending System Prompt")
-            // }
+            if (this.debug) {
+                console.log("Done Sending System Prompt")
+            }
         }
-        // if(this.debug){
-        //     console.log("Started Taking Snapshot")
-        // }
-        // var state: DomState = await this.domService.getClickableElements(this.highlightElements, -1, 0);
+        if (this.debug) {
+            console.log("Started Taking Snapshot")
+        }
         const snapshot = await this.page.locator('body').ariaSnapshot({
             ref: true,
         })
         const str = snapshot
         try {
             var start = new Date().getTime()
-            // if (this.debug){
-            //     console.log("Snapshot: ",str)
-            // }
+            if (this.debug) {
+                console.log("Snapshot: ", str)
+            }
             const res = await this.model.invoke([
                 [
                     "human",
@@ -136,16 +185,15 @@ export class AI {
             ]);
 
             var end = new Date().getTime()
-            // if (this.debug){
-            //     console.log("AI Response: ",res)
-            //     console.log("Time Taken: "+((end-start)/1000))
-            // }
+            if (this.debug) {
+                console.log("AI Response: ", res)
+                console.log("Time Taken: " + ((end - start) / 1000))
+            }
             for (const call of res.tool_calls) {
                 switch (call.name) {
                     case "navigate_to_url":
                         await page.goto(call.args.url)
                         break
-
                     case "click_on_element":
                         await page.click(`aria-ref=${call.args.ref}`)
                         break
@@ -154,6 +202,11 @@ export class AI {
                         break
                     case "assert_if_element_is_visible":
                         return !(call.args.ref == null || call.args.ref == undefined)
+                    case "drag_element_and_drop_on_element":
+                        const sourceRef = call.args.sourceRef
+                        const targetRef = call.args.targetRef
+                        await page.dragAndDrop(`aria-ref=${sourceRef}`, `aria-ref=${targetRef}`)
+                        break
                 }
             }
         } catch (e) {
